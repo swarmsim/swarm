b756388e815ae631da029e6f055aa66901a56f6c
Evan Rosson <github@erosson.org>
2014-09-02 18:18:40 -0700

    remove dev url from source. fixes #54

--- serve.sh
+++ serve.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -eux
+echo $NGROK_SWARMSIM > /dev/null #fail undefined check early
 grunt="serve"
 if [ $# -ge 1 ]; then
   grunt="$1";shift
 fi
 parallel -j 2 --tag --linebuffer <<EOF
 grunt $grunt
-ngrok -log stdout -subdomain=swarm-demo 9000 > /dev/null
+ngrok -log stdout -subdomain=$NGROK_SWARMSIM 9000 > /dev/null
 EOF
