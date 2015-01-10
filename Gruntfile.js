67d791a86af7da74204e4d87b691392a4b30734b
Evan Rosson <github@erosson.org>
2015-01-10 13:35:55 -0800

    disable spreadsheet updates for 0.2.x branch. really shoudl've made a new sheet...

--- Gruntfile.js
+++ Gruntfile.js
@@ -238,7 +238,8 @@ module.exports = function (grunt) {
           ]
         }]
       },
-      spreadsheetpreload: 'app/scripts/spreadsheetpreload',
+      //spreadsheetpreload: 'app/scripts/spreadsheetpreload',
+      spreadsheetpreload: 'app/scripts/spreadsheetpreload/v0.1.js',
       server: '.tmp'
     },
 
@@ -529,6 +530,8 @@ module.exports = function (grunt) {
 
   // One of few swarmapp-specific tasks
   grunt.registerMultiTask('preloadSpreadsheet', 'Update spreadsheet data', function () {
+    // TODO: spreadsheet updates disabled for 0.2.x branch. Remove this when merging!
+    return;
     var Tabletop = require('tabletop');
     var stringify = require('json-stable-stringify');
     var _ = require('lodash');
