43c4b8d1518f72c172937cfd7fc3bb9725e29dd6
Evan Rosson <github@erosson.org>
2015-04-05 20:35:01 -0700

    caching headers

--- config/http.js
+++ config/http.js
@@ -65,6 +65,7 @@ module.exports.http = {
     accessControlAllowOrigin: function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Cache-Control" "private, max-age=0, no-cache");
       next();
     },
 
