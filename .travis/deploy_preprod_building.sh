60a125819858007f065cdf6f590070e0c42899b8
Evan Rosson <github@swarmsim.com>
2015-04-09 17:13:49 -0700

    more fiddling with the releasewatch page. github pages doesn't like _prefixes, so use /releasewatch instead. #540

--- .travis/deploy_preprod_building.sh
+++ .travis/deploy_preprod_building.sh
@@ -5,13 +5,14 @@ remote='git@github.com:swarmsim-preprod/swarmsim-preprod.github.io.git'
 git clone --depth 1 $remote .travis/tmp/repo
 
 cd .travis/tmp/repo
-rm -rf _releasing
-cp -rp ../../_releasing _releasing
-sed -i "s/<%= date %>/`date`/" ../../_releasing/index.html
+releasedir=releasewatch
+rm -rf $releasedir
+cp -rp ../../$releasedir $releasedir
+sed -i "s/<%= date %>/`date`/" ../../$releasedir/index.html
 git add .
 git config user.name "Evan Rosson (via travis-ci.org robots)"
 git config user.email `echo "genivf-pv@fjnezfvz.pbz" | tr "[A-Za-z]" "[N-ZA-Mn-za-m]"`
-git commit -m 'build started. visit /_release/ in a browser to watch for completion.'
+git commit -m "build started. visit /$releasedir/ in a browser to watch for completion."
 git push
 cd -
 
