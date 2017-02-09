1e6e101c49d935fc88bcd1f07386dd41363fb199
Evan <github@erosson.org>
2017-02-08 21:32:05 -0500

    re-enable spreadsheet

--- Gruntfile.js
+++ Gruntfile.js
@@ -763,7 +763,6 @@ module.exports = function (grunt) {
 
   // One of few swarmapp-specific tasks
   grunt.registerMultiTask('preloadSpreadsheet', 'Update spreadsheet data', function () {
-    return;
     var Tabletop = require('tabletop');
     var stringify = require('json-stable-stringify');
     var _ = require('lodash');
