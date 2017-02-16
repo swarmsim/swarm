19c1cb2448bf9549edc791cf27e1e8343bb3ca65
Evan <github@erosson.org>
2017-02-16 17:04:02 -0500

    playfab upload files

---
+++ app/scripts/spreadsheetpreload/v0.2.playfabUpload.beta.js
@@ -0,0 +1,45 @@
+{
+  "Catalog": [
+    {
+      "Description": "50,000 crystals",
+      "DisplayName": "50,000 crystals",
+      "ItemId": "crystal001",
+      "VirtualCurrencyPrices": {
+        "RM": 499
+      }
+    },
+    {
+      "Description": "110,000 crystals",
+      "DisplayName": "110,000 crystals",
+      "ItemId": "crystal002",
+      "VirtualCurrencyPrices": {
+        "RM": 999
+      }
+    },
+    {
+      "Description": "250,000 crystals",
+      "DisplayName": "250,000 crystals",
+      "ItemId": "crystal003",
+      "VirtualCurrencyPrices": {
+        "RM": 1999
+      }
+    },
+    {
+      "Description": "700,000 crystals",
+      "DisplayName": "700,000 crystals",
+      "ItemId": "crystal004",
+      "VirtualCurrencyPrices": {
+        "RM": 4999
+      }
+    },
+    {
+      "Description": "1,500,000 crystals",
+      "DisplayName": "1,500,000 crystals",
+      "ItemId": "crystal005",
+      "VirtualCurrencyPrices": {
+        "RM": 9999
+      }
+    }
+  ],
+  "CatalogVersion": "beta"
+}
---
+++ app/scripts/spreadsheetpreload/v0.2.playfabUpload.free.js
@@ -0,0 +1,45 @@
+{
+  "Catalog": [
+    {
+      "Description": "50,000 crystals",
+      "DisplayName": "50,000 crystals (free)",
+      "ItemId": "crystal001",
+      "VirtualCurrencyPrices": {
+        "RM": 0
+      }
+    },
+    {
+      "Description": "110,000 crystals",
+      "DisplayName": "110,000 crystals (free)",
+      "ItemId": "crystal002",
+      "VirtualCurrencyPrices": {
+        "RM": 0
+      }
+    },
+    {
+      "Description": "250,000 crystals",
+      "DisplayName": "250,000 crystals (free)",
+      "ItemId": "crystal003",
+      "VirtualCurrencyPrices": {
+        "RM": 0
+      }
+    },
+    {
+      "Description": "700,000 crystals",
+      "DisplayName": "700,000 crystals (free)",
+      "ItemId": "crystal004",
+      "VirtualCurrencyPrices": {
+        "RM": 0
+      }
+    },
+    {
+      "Description": "1,500,000 crystals",
+      "DisplayName": "1,500,000 crystals (free)",
+      "ItemId": "crystal005",
+      "VirtualCurrencyPrices": {
+        "RM": 0
+      }
+    }
+  ],
+  "CatalogVersion": "free"
+}
