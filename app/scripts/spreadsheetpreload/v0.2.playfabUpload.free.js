4a52901dcfb53741a341b145a0b8870f8cf00113
Evan <github@erosson.org>
2017-02-17 16:54:11 -0500

    0.99 crystals

--- app/scripts/spreadsheetpreload/v0.2.playfabUpload.free.js
+++ app/scripts/spreadsheetpreload/v0.2.playfabUpload.free.js
@@ -1,32 +1,40 @@
 {
   "Catalog": [
     {
-      "Description": "50,000 crystals",
-      "DisplayName": "50,000 crystals (free)",
+      "Description": "10,000 crystals",
+      "DisplayName": "10,000 crystals (free)",
+      "ItemId": "crystal000",
+      "VirtualCurrencyPrices": {
+        "RM": 0
+      }
+    },
+    {
+      "Description": "55,000 crystals",
+      "DisplayName": "55,000 crystals (free)",
       "ItemId": "crystal001",
       "VirtualCurrencyPrices": {
         "RM": 0
       }
     },
     {
-      "Description": "110,000 crystals",
-      "DisplayName": "110,000 crystals (free)",
+      "Description": "120,000 crystals",
+      "DisplayName": "120,000 crystals (free)",
       "ItemId": "crystal002",
       "VirtualCurrencyPrices": {
         "RM": 0
       }
     },
     {
-      "Description": "250,000 crystals",
-      "DisplayName": "250,000 crystals (free)",
+      "Description": "260,000 crystals",
+      "DisplayName": "260,000 crystals (free)",
       "ItemId": "crystal003",
       "VirtualCurrencyPrices": {
         "RM": 0
       }
     },
     {
-      "Description": "700,000 crystals",
-      "DisplayName": "700,000 crystals (free)",
+      "Description": "560,000 crystals",
+      "DisplayName": "560,000 crystals (free)",
       "ItemId": "crystal004",
       "VirtualCurrencyPrices": {
         "RM": 0
--- app/scripts/spreadsheetpreload/v0.2.playfabUpload.main.js
+++ app/scripts/spreadsheetpreload/v0.2.playfabUpload.main.js
@@ -1,35 +1,43 @@
 {
   "Catalog": [
     {
-      "Description": "50,000 crystals",
-      "DisplayName": "50,000 crystals",
+      "Description": "10,000 crystals",
+      "DisplayName": "10,000 crystals",
+      "ItemId": "crystal000",
+      "VirtualCurrencyPrices": {
+        "RM": 99
+      }
+    },
+    {
+      "Description": "55,000 crystals",
+      "DisplayName": "55,000 crystals",
       "ItemId": "crystal001",
       "VirtualCurrencyPrices": {
         "RM": 499
       }
     },
     {
-      "Description": "110,000 crystals",
-      "DisplayName": "110,000 crystals",
+      "Description": "120,000 crystals",
+      "DisplayName": "120,000 crystals",
       "ItemId": "crystal002",
       "VirtualCurrencyPrices": {
         "RM": 999
       }
     },
     {
-      "Description": "250,000 crystals",
-      "DisplayName": "250,000 crystals",
+      "Description": "260,000 crystals",
+      "DisplayName": "260,000 crystals",
       "ItemId": "crystal003",
       "VirtualCurrencyPrices": {
         "RM": 1999
       }
     },
     {
-      "Description": "700,000 crystals",
-      "DisplayName": "700,000 crystals",
+      "Description": "560,000 crystals",
+      "DisplayName": "560,000 crystals",
       "ItemId": "crystal004",
       "VirtualCurrencyPrices": {
-        "RM": 4999
+        "RM": 3999
       }
     },
     {
