a97aecffec6704fcc167f1b6cda4c2d1da9077b3
Evan Rosson <git@erosson.org>
2022-12-09 23:54:22 +0000

    jsdom

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import {Config} from 'jest'
 
 const config: Config = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom',
     testMatch: ["<rootDir>/tests/**/*.test.*"],
     // https://stackoverflow.com/questions/58613492/how-to-resolve-cannot-use-import-statement-outside-a-module-in-jest
     transform: {
--- tests/version.test.ts
+++ tests/version.test.ts
@@ -1,6 +1,3 @@
-/**
- * @jest-environment jsdom
- */
 import '../src/main'
 
 test('runs', () => {
