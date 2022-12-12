5f7f12fb4ab2a601deefc8b45272005dcbe66ef6
Evan Rosson <git@erosson.org>
2022-12-12 00:11:55 +0000

    planning

---
+++ swarmsim-ts/src/spreadsheet/data.ts
@@ -0,0 +1,7 @@
+import unittypes from "../unittype/data";
+import upgrades from "../upgrade/data";
+import achievements from "../achievement/data";
+import mtx from "../mtx/data";
+
+// TODO match spreadsheet format; column names
+export default { unittypes, upgrades, achievements, mtx };
