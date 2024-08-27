d18d2e154a9f8231aad8bc867a25393fa68a0355
Evan Rosson <git@erosson.org>
2024-08-27 05:04:22 +0000

    fix ruby/compass install, and devcontainer/docker/ci build

--- .devcontainer/setup/rvm.sh
+++ .devcontainer/setup/rvm.sh
@@ -12,6 +12,9 @@ source $(rvm $RUBY_VERSION do rvm env --path)
 rvm alias delete default
 rvm alias create default $RUBY_VERSION
 
+# compass tries to install ffi 1.17.0 by default (as of 2024/08/27), but that fails because we're pinned to such old versions of everything.
+# manually installing 1.16.0 seems to work.
+gem install ffi -v 1.16.0  
 gem install compass
 
 ruby --version
